a5cccd4ee3b8953507f804cd7c1e207d2cec2540
JahnaviSripada <121272130+JahnaviSripada@users.noreply.github.com>
2022-12-24 21:20:30 +0530

    complete captcha project
    create captcha service for a webpage

---
+++ website/app.js
@@ -0,0 +1,36 @@
+(function () {
+    const fonts = ["cursive", "sans-serif", "serif", "monospace"];
+    let captchaValue = "";
+    function generateCaptcha() {
+        let value = window.btoa(Math.random() * 10000000);
+        value = value.substring(0, 5 + Math.random() * 5);
+        captchaValue = value;
+    }
+    function setCaptcha() {
+        let html = captchaValue.split("").map((char) => {
+            const rotate = -20 + Math.trunc(Math.random() * 30);
+            const font = Math.trunc(Math.random() * fonts.length);
+            return `<span style="transform:rotate(${rotate}deg);
+            font-family:${fonts[font]}">${char}</span>`;
+        }).join("");
+        document.querySelector(".login-form .captcha .preview").innerHTML = html;
+    }
+    function initCaptcha() {
+        document.querySelector(".login-form .captcha .captcha-refresh").addEventListener("click", function () {
+            generateCaptcha();
+            setCaptcha();
+        });
+        generateCaptcha();
+        setCaptcha();
+
+    }
+    initCaptcha();
+    document.querySelector(".login-form #login-btn").addEventListener("click", function () {
+        let inputCaptchaValue = document.querySelector(".login-form .captcha input").value;
+        if (inputCaptchaValue === captchaValue) {
+            swal("", "Logging In!", "Success");
+        } else {
+            swal("Invalid Captcha");
+        }
+    });
+})();
